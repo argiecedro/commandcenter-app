a3d3bbc752ed4593086c51642cf1b7c23c5ddc5f
argiecedro <65527518+argiecedro@users.noreply.github.com>
2023-03-06 14:42:06 +0800

    office
    new

---
+++ app/Http/Livewire/Office.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Office extends Component
+{
+    public function render()
+    {
+        return view('livewire.office');
+    }
+}
