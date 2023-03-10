1160b72a00193baa92ebe01f79c19d0a40bf0b99
argiecedro <65527518+argiecedro@users.noreply.github.com>
2023-03-10 17:22:23 +0800

    home
    temp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,19 +8,24 @@
      @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body x-data="{open: false}">
+<body>
 
-  <header class="flex justify-between text-black py-4 px-4">
 
-    <a href="/" class="text-lg font-bold">Command Center</a>
+<div x-data="{open: false}" class="top-0 sticky items-center">
 
+  <header class="flex text-black py-4 px-4 bg-gray-700 opacity-95 justify-between">
     
+ 
+      <a href="/" class="text-lg font-bold items-center mr-28">Command Center</a>
+      
 
-    <ul class="flex flex-row items-center align-middle gap-2">
-      <li><a href="#" class="bg-purple-400 text-white py-2 px-2 rounded-lg float-right">Account</a></li>
-    </ul>
+    <ul class="flex flex-row items-center gap-2">
+      
+      <li><a href="#" class="bg-purple-400 text-white py-2 px-2 rounded-lg">Account</a></li>
 
-    <button
+      
+
+          <button
     
     x-on:click="open = ! open"
     class="md:hidden">
@@ -37,18 +42,37 @@
       </svg>
       
     </button>
+    </ul>
+
+
 
   </header>
 
   <div
   
   x-bind:class="open ? 'fixed w-64 opacity-95' : 'hidden'"
-  class="md:block md:fixed md:w-64 h-screen bg-green-400 px-4 py-4">
-    sidebar
-  </div>
+  class="md:block md:fixed md:w-64 bg-green-400 h-screen">
+    <div>
+      <x-testnav/>
 
-  <div class="border-2 md:ml-64 p-2">
-      <div class="bg-red-400 px-4 py-2 items-center">
+      <div>
+        <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 group-hover:fill-cyan-600" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
+          </svg>
+          <span class="group-hover:text-gray-700">Settings</span>
+        </a>
+      </div>
+    </div>
+    
+  </div>
+</div>
+
+
+  <div class="border-2 md:ml-64 p-2 mx-auto overscroll-auto flex-auto">
+
+    <x-modal></x-modal>
+      <div class="bg-red-400 px-4 py-2 items-center overflow-auto">
         <table class="table-auto">
           <thead>
             <tr>
@@ -76,6 +100,271 @@
           </tbody>
         </table>
       </div>
+
+      <div class="bg-red-400 px-4 py-2 items-center overflow-auto">
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="bg-red-400 px-4 py-2 items-center overflow-auto">
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="bg-red-400 px-4 py-2 items-center overflow-auto">
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="bg-red-400 px-4 py-2 items-center overflow-auto">
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="bg-red-400 px-4 py-2 items-center overflow-auto">
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="bg-red-400 px-4 py-2 items-center overflow-auto">
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="bg-red-400 px-4 py-2 items-center overflow-auto">
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="bg-red-400 px-4 py-2 items-center overflow-auto">
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="bg-red-400 px-4 py-2 items-center overflow-auto">
+        <table class="table-auto">
+          <thead>
+            <tr>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      
+
+
+
   </div>
 
 
