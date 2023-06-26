e5e54df9b7d2ef5fffb4c46ecd72f05d8c4f0093
argiecedro <65527518+argiecedro@users.noreply.github.com>
2023-06-26 16:24:04 +0800

    cancel

---
+++ resources/views/components/button/cancel.blade.php
@@ -0,0 +1,8 @@
+
+
+<button type="button" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2 -ml-1 inline-flex">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+  </svg>
+  {{ $slot }}
+</button>
--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -15,11 +15,9 @@
 
     <div class="flex">
         
-        <button type="button" class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-4 py-2.5 text-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2 -ml-1 inline-flex">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-              </svg>Add Items
-        </button>
+        <x-button.cancel>
+            Cancel
+        </x-button.cancel>
     </div>
     
     <div class="flex">
